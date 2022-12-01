605671d63bfd2497b8e0bdd225e36fdd3df36458
Uri Mtz <elgudi10@hotmail.com>
2022-11-30 23:39:59 -0600

    Agregado General11

---
+++ General11.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+
+public class General11 {
+
+  public static void main(String[] args) {
+
+    Scanner obj = new Scanner(System.in);
+    obj.useDelimiter("\n");
+
+    String letras = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+
+    System.out.println("Dame una frase");
+    String frase = obj.next();
+
+    obj.close();
+
+    String texto = codificar(letras, frase);
+    System.out.println("Texto codificado: " + texto);
+
+    texto = descodificar(letras, texto);
+    System.out.println("Texto descodificado: " + texto);
+
+  }
+
+  public static String codificar(String letras, String texto) {
+    String textoCodificado = "";
+
+    texto = texto.toUpperCase();
+
+    char caracter;
+    for (int i = 0; i < texto.length(); i++) {
+      caracter = texto.charAt(i);
+
+      int pos = letras.indexOf(caracter);
+
+      if (pos == -1) {
+        textoCodificado += caracter;
+      } else {
+        textoCodificado += letras.charAt((pos + 3) % letras.length());
+      }
+
+    }
+
+    return textoCodificado;
+  }
+
+  public static String descodificar(String letras, String texto) {
+    String textoDescodificado = "";
+
+    texto = texto.toUpperCase();
+
+    char caracter;
+    for (int i = 0; i < texto.length(); i++) {
+      caracter = texto.charAt(i);
+
+      int pos = letras.indexOf(caracter);
+
+      if (pos == -1) {
+        textoDescodificado += caracter;
+      } else {
+        if (pos - 3 < 0) {
+          textoDescodificado += letras.charAt(letras.length() + (pos - 3));
+        } else {
+          textoDescodificado += letras.charAt((pos - 3) % letras.length());
+        }
+      }
+
+    }
+
+    return textoDescodificado;
+  }
+}
